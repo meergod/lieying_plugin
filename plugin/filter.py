ff67f36304be012a4df0e9555811893987e8917f
sceext <sceext@foxmail.com>
2015-07-13 10:36:52 +0800

    plugin/main.py, ready to test GetVersion()

---
+++ plugin/filter.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# filter.py for lieying_plugin/you-get (parse)
+# plugin/filter: define filter for GetVersion()
+# version 0.0.1.0 test201507131031
+
+# global vars
+
+LIEYING_PLUGIN_FILTER = [
+    '^http://.+', 
+]
+
+# function
+def get_filter():
+    # no more to do, just return it
+    return LIEYING_PLUGIN_FILTER
+
+# end filter.py
+
+
---
+++ plugin/main.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# main.py for lieying_plugin/you-get (parse)
+# plugin/main: plugin main file. 
+# version 0.0.1.0 test201507131036
+
+# import
+
+import json
+
+from . import version
+from . import filter as filter0
+
+from . import conf
+from . import tinfo
+from . import run_sub
+
+from . import parse_you_get as parse0
+
+# function
+
+# lieying_plugin functions
+
+def lieying_plugin_GetVersion():
+    out = {}	# output info obj
+    
+    out['port_version'] = version.LIEYING_PLUGIN_PORT_VERSION
+    out['type'] = version.LIEYING_PLUGIN_TYPE
+    out['uuid'] = version.LIEYING_PLUGIN_UUID
+    out['version'] = version.LIEYING_PLUGIN_VERSION
+    
+    out['name'] = version.make_plugin_name()
+    out['filter'] = filter0.get_filter()
+    
+    out['author'] = version.THIS_AUTHOR
+    out['license'] = version.THIS_LICENSE
+    out['home'] = version.THIS_HOME
+    out['note'] = version.THIS_NOTE
+    
+    # done
+    text = json.dumps(out)
+    return text
+
+def lieying_plugin_StartConfig():
+    raise Exception('lieying_plugin/you-get: ERROR: [StartConfig()] not support config now. ')
+
+def lieying_plugin_Parse(input_text):
+    pass
+
+def lieying_plugin_ParseURL(url, label, i_min=None, i_max=None):
+    pass
+
+# end main.py
+
+
