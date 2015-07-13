3eb3887e8aa7a2e5003c832079c50a8cc89d65aa
sceext <sceext@foxmail.com>
2015-07-13 15:26:02 +0800

    plugin/parse_you_get.py, tinfo.py, finished, ready to test Parse() and ParseURL() with you-get

--- plugin/parse_you_get.py
+++ plugin/parse_you_get.py
@@ -1,19 +1,75 @@
 # -*- coding: utf-8 -*-
 # parse_you_get.py for lieying_plugin/you-get (parse)
 # plugin/parse_you_get: parse output text of you-get
-# version 0.0.0.1 test201507131441
+# version 0.0.1.0 test201507131510
 
-# import
+# base function
+
+# clean chars before text
+def clean_before(text, to_clean=' \r\n	'):
+    t = text
+    while (len(t) > 0) and (t[0] in to_clean):
+        t = t[1:]
+    return t
 
 # function
 
 # parse --info output
 def parse_info(raw_text):
-    pass
+    
+    raw = {}	# raw info
+    
+    fill_list = [
+        'site', 
+        'title', 
+        'type', 
+        'size', 
+    ]
+    # fill null info
+    for f in fill_list:
+        raw[f] = ''
+    
+    # cut text in lines
+    line = raw_text.split('\n')
+    
+    # process key list
+    key_list = {
+        'Video Site' : 'site', 
+        'Title' : 'title', 
+        'Type' : 'type', 
+        'Size' : 'size', 
+    }
+    
+    # process each line
+    for l in line:
+        try:	# split may failed
+            l0, l1 = l.split(':', 1)
+        except Exception:
+            continue	# just ignore this line
+        
+        for k in key_list:
+            if l0 == k:
+                raw[key_list[k]] = clean_before(l1)
+    # process raw info done
+    return raw
 
 # parse --url output
 def parse_url(raw_text):
-    pass
+    
+    # get Real URLs part
+    part = raw_text.split('Real URLs:', 1)
+    
+    # cut in lines
+    line = part[1].split('\n')
+    
+    raw = []	# raw info
+    
+    # process each line
+    for l in line:
+        if l != '':	# check not null line
+            raw.append(l)
+    # parse raw done
+    return raw
 
 # end parse_you_get.py
 
--- plugin/tinfo.py
+++ plugin/tinfo.py
@@ -1,19 +1,70 @@
 # -*- coding: utf-8 -*-
 # tinfo.py for lieying_plugin/you-get (parse)
 # plugin/tinfo: translate info to plugin output format. 
-# version 0.0.0.1 test201507131443
+# version 0.0.1.0 test201507131525
 
 # import
+
+# global vars
+TYPE_TO_EXT_LIST = {
+    'video/x-flv' : 'flv', 
+}
+
+# base functions
+
+def get_you_get_ext(type_text):
+    
+    # get MIME format text
+    mime = type_text.split('(', 1)[1].split(')', 1)[0]
+    
+    # use TYPE_TO_EXT_LIST to get ext
+    ext = TYPE_TO_EXT_LIST[mime]
+    
+    return ext
 
 # function
 
 # translate for Parse() one video output
 def t_format(raw_info):
-    pass
+    raw = raw_info
+    out = {}	# output info
+    
+    out['type'] = 'formats'
+    out['name'] = raw['title'] + '_' + raw['site']
+    
+    # only support one output format
+    one = {}
+    out['data'] = [one]
+    
+    # fix size string, MiB -> MB
+    size = raw['size'].replace('iB', 'B', 1)
+    one['size'] = size
+    
+    # get ext
+    ext = get_you_get_ext(raw['type'])
+    one['ext'] = ext
+    
+    # make label
+    label = 'default_' + raw['type']
+    one['label'] = label
+    
+    # done
+    return out
 
 # translate for ParseURL() output
 def t_url(raw_info):
-    pass
+    raw = raw_info
+    
+    out = []
+    # process each url, just add it
+    for u in raw:
+        one = {}
+        one['protocol'] = 'http'
+        one['args'] = {}
+        one['value'] = u
+        out.append(one)
+    # done
+    return out
 
 # end tinfo.py
 
