35d15f3974f4d095b840c196d53477cb6c3b8bdd
sceext <sceext@foxmail.com>
2015-07-22 12:29:10 +0800

    lieying_plugin_youtube-dl, version 0.1.0

--- plugin/version.py
+++ plugin/version.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # version.py for lieying_plugin/youtube-dl (parse)
 # plugin/version: define info for GetVersion()
-# version 0.0.14.0 test201507201944
+# version 0.1.0.0 test201507221228
 
 # import
 from . import conf
@@ -9,13 +9,13 @@ from . import run_sub
 
 # global vars
 
-THIS_PACK_VERSION = '1'
+THIS_PACK_VERSION = '2'
 
 LIEYING_PLUGIN_PORT_VERSION = '0.2.1'
 LIEYING_PLUGIN_TYPE = 'parse'
 
 LIEYING_PLUGIN_UUID = '88e47809-089c-45a9-b097-5087c260588f'
-LIEYING_PLUGIN_VERSION = '0.0.1'
+LIEYING_PLUGIN_VERSION = '0.1.0'
 
 THIS_AUTHOR = 'sceext <sceext@foxmail.com>'
 THIS_LICENSE = 'unlicense <http://unlicense.org> and FreeBSD License'
