99a4e668d53654e36f9dbd726eb2030634f53529
sceext <sceext@gmail.com>
2015-08-19 01:40:01 +0800

    lib/conf.py, enable use nosalt by default

--- src/lib/conf.py
+++ src/lib/conf.py
@@ -1,6 +1,6 @@
 # conf.py for lyp_bridge, lib/conf, simple config file, sceext <sceext@foxmail.com> 
 
-enable_nosalt = False
+enable_nosalt = True
 
 # path from plugin root path
 slimerjs_bin = 'slimerjs/slimerjs.bat'
